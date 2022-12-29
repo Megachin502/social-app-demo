39398fd324cb2f7d9921ab4d7735e2117410c9ca
Elvis Lam <elvislam502@gmail.com>
2022-12-28 21:30:12 -0800

    u

--- pages/api/flutter/index.js
+++ pages/api/flutter/index.js
@@ -1,80 +1,81 @@
-import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0'
 
 export default withApiAuthRequired(async function handler(req, res) {
-  const { accessToken } = await getAccessToken(req, res);
+  const { accessToken } = await getAccessToken(req, res)
   const fetchOptions = {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Request-Headers": "*",
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
       jwtTokenString: accessToken,
     },
-  };
+  }
   const fetchBody = {
     dataSource: process.env.MONGODB_DATA_SOURCE,
-    database: "social_butterfly",
-    collection: "flutters",
-  };
-  const baseUrl = `${process.env.MONGODB_DATA_API_URL}/action`;
+    database: 'social_butterfly',
+    collection: 'flutters',
+  }
+  const baseUrl = `${process.env.MONGODB_DATA_API_URL}/action`
 
   try {
     switch (req.method) {
-      case "GET":
+      case 'GET':
         const readData = await fetch(`${baseUrl}/find`, {
           ...fetchOptions,
           body: JSON.stringify({
             ...fetchBody,
             sort: { postedAt: -1 },
           }),
-        });
-        const readDataJson = await readData.json();
-        res.status(200).json(readDataJson.documents);
-        break;
-      case "POST":
-        const flutter = req.body;
+        })
+        const readDataJson = await readData.json()
+        res.status(200).json(readDataJson.documents)
+        break
+      case 'POST':
+        const flutter = req.body
         const insertData = await fetch(`${baseUrl}/insertOne`, {
           ...fetchOptions,
           body: JSON.stringify({
             ...fetchBody,
             document: flutter,
           }),
-        });
-        const insertDataJson = await insertData.json();
-        res.status(200).json(insertDataJson);
-        break;
-      case "PUT":
+        })
+        const insertDataJson = await insertData.json()
+        res.status(200).json(insertDataJson)
+        break
+      case 'PUT':
         const updateData = await fetch(`${baseUrl}/updateOne`, {
           ...fetchOptions,
           body: JSON.stringify({
             ...fetchBody,
             filter: { _id: { $oid: req.body._id } },
             update: {
-              $set: {
-                body: req.body.body,
+              [req.body.action]: {
+                likes: req.body.userId,
               },
             },
           }),
-        });
-        const updateDataJson = await updateData.json();
-        res.status(200).json(updateDataJson);
-        break;
-      case "DELETE":
+        })
+        const updateDataJson = await updateData.json()
+        res.status(200).json(updateDataJson)
+        break
+      case 'DELETE':
         const deleteData = await fetch(`${baseUrl}/deleteOne`, {
           ...fetchOptions,
           body: JSON.stringify({
             ...fetchBody,
             filter: { _id: { $oid: req.body._id } },
           }),
-        });
-        const deleteDataJson = await deleteData.json();
-        res.status(200).json(deleteDataJson);
-        break;
+        })
+        const deleteDataJson = await deleteData.json()
+        res.status(200).json(deleteDataJson)
+        break
+
       default:
-        res.status(405).end();
-        break;
+        res.status(405).end()
+        break
     }
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error });
+    console.error(error)
+    res.status(500).json({ error })
   }
-});
+})
